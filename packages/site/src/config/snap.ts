e1767767a16abfca5e8940f93193554c02bd4a0b
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-07-03 17:38:22 +0300

    fix

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,5 +2,5 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-export const defaultSnapOrigin = 'https://snaps.polygonid.me';
+export const defaultSnapOrigin = 'web:https://snaps.polygonid.me';
 export const CIRCUITS_FETCH_PATH ='https://snaps.polygonid.me';
