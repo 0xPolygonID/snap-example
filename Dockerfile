eef0d459f565b0ebd55ef454dbb9467d3aa2bbaf
Dimasik Kolezhniuk <d.kolezhiniyk@gmail.com>
2023-07-03 19:04:34 +0200

    prepare snap npm publishing

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@
 FROM nginx:1.17.1-alpine
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY packages/site/public /usr/share/nginx/html
-COPY packages/snap/dist /usr/share/nginx/html/dist
-COPY packages/snap/images /usr/share/nginx/html/images
-COPY packages/snap/snap.manifest.json /usr/share/nginx/html
--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,5 +2,5 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-export const defaultSnapOrigin = 'local:https://snaps.polygonid.me';
-export const CIRCUITS_FETCH_PATH ='https://snaps.polygonid.me';
+export const defaultSnapOrigin = 'npm:@0xpolygonid/snap-example';
+export const CIRCUITS_FETCH_PATH = 'https://snaps.polygonid.me';
