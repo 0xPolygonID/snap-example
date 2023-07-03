491261115b874324fc72f15956f92304b9ca66ba
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-07-03 17:15:02 +0300

    harcode url

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,7 +2,5 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-export const defaultSnapOrigin =
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
-export const CIRCUITS_FETCH_PATH =
-  process.env.CIRCUITS_FETCH_PATH ?? 'http://localhost:8000';
+export const defaultSnapOrigin = 'https://snaps.polygonid.me';
+export const CIRCUITS_FETCH_PATH ='https://snaps.polygonid.me';
